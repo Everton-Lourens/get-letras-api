101ce858a9b296acd0b1f2e0230cedb54426e830
Everton-Lourens <123413870+Everton-Lourens@users.noreply.github.com>
2023-05-23 12:37:35 -0300

    Implementando writeFile no txt

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
+
+query();
+
+async function query() {
+   try {
+      const filePath = './letra.txt'; // Caminho do arquivo de texto
+      const pesquisarLetra = fs.readFileSync(filePath, 'utf8');
+
+      //console.log(pesquisarLetra + ' site:letras.mus.br');
+      searchOnGoogle(pesquisarLetra + ' site:letras.mus.br');
+
+   } catch (error) {
+      console.error('Erro ao ler o arquivo:', error);
+   }
+}
 
 async function searchOnGoogle(query) {
    const baseUrl = 'https://www.google.com/search';
@@ -13,13 +29,13 @@ async function searchOnGoogle(query) {
       if (match && match[1]) {
          const firstLink = decodeURIComponent(match[1]);
 
-         console.log(firstLink);
+         //console.log(firstLink);
 
          if (firstLink) {
             try {
                const response = await axios.get(firstLink);
                console.log('1111111111111');
-               const searchResults = parseSearchResponseToList(response.data);
+               parseSearchResponseToList(response.data);
                console.log('2222222222222');
             } catch (error) {
                console.error('Erro ao pesquisar letra:', error);
@@ -35,73 +51,57 @@ async function searchOnGoogle(query) {
 
 function parseSearchResponseToList(html) {
    try {
-     let $ = cheerio.load(html);
-     const titleElement = $('title');
-     const titleName = titleElement.text();
-     const titleAndArtist = titleName.replace(' - LETRAS.MUS.BR', '');
- 
-     const splitTitleAndArtist = titleAndArtist.split(' - ');
- 
-     const title = splitTitleAndArtist[0];
-     const artist = splitTitleAndArtist[1];
- 
-     //console.log(title);
-     //console.log(artist);
- 
-     // Selecionar o elemento que contém a letra da música
-     const letraContainer = $('.cnt-letra');
- 
-     // Extrair o texto bruto da letra
-     let letraBruta = letraContainer.html();
- 
-     // Substituir <br> por \n
-     letraBruta = letraBruta.replace(/<br\s*\/?>/gi, '\n');
- 
-     // Substituir <p> por \n\n
-     letraBruta = letraBruta.replace(/<\/?p[^>]*>/gi, '\n');
- 
-     // Remover tags restantes
-     letraBruta = letraBruta.replace(/<\/?[^>]+(>|$)/g, '');
- 
-     console.log(letraBruta.trim());
- 
+      let $ = cheerio.load(html);
+      const titleElement = $('title');
+      const titleName = titleElement.text();
+      const titleAndArtist = titleName.replace(' - LETRAS.MUS.BR', '');
+
+      const splitTitleAndArtist = titleAndArtist.split(' - ');
+
+      const title = splitTitleAndArtist[0];
+      const artist = splitTitleAndArtist[1];
+
+      console.log(title);
+      console.log(artist);
+
+      // Selecionar o elemento que contém a letra da música
+      const letraContainer = $('.cnt-letra');
+
+      // Extrair o texto bruto da letra
+      let letraBruta = letraContainer.html();
+
+      // Substituir <br> por \n
+      letraBruta = letraBruta.replace(/<br\s*\/?>/gi, '\n');
+
+      // Substituir <p> por \n\n
+      letraBruta = letraBruta.replace(/<\/?p[^>]*>/gi, '\n');
+
+      // Remover tags restantes
+      letraBruta = letraBruta.replace(/<\/?[^>]+(>|$)/g, '');
+
+      console.log(letraBruta.trim());
+
+      try {
+         const filePath = './letra.txt'; // Caminho do arquivo de texto
+         const conteudoParaEscrever = letraBruta.trim();
+   
+         // Opção 1: Escrever de forma síncrona
+         fs.writeFileSync(filePath, conteudoParaEscrever, 'utf8');
+         console.log('Conteúdo escrito no arquivo com sucesso.');
+   
+         // Opção 2: Escrever de forma assíncrona
+         fs.writeFile(filePath, conteudoParaEscrever, 'utf8', (error) => {
+            if (error) {
+               console.error('Erro ao escrever no arquivo:', error);
+            } else {
+               console.log('Conteúdo escrito no arquivo com sucesso.');
+            }
+         });
+      } catch (error) {
+         console.error('Erro ao escrever no arquivo:', error);
+      }
+
    } catch (error) {
-     console.error('Ocorreu um erro:', error);
+      console.error('Ocorreu um erro:', error);
    }
- }
-
-const query = 'seu sangue' + ' site:letras.mus.br';
-searchOnGoogle(query);
-
-
- //parseSearchResponseToList(html);
-
-/*
-   //const $ = cheerio.load(html);
-   const searchResults = [];
-
-   $('.search-results .cnt-list .cnt-list-row').each((index, element) => {
-      const $element = $(element);
-      const id = $element.find('.cnt-list-row .cnt-list-row a').attr('href');
-      const title = $element.find('.cnt-list-row .cnt-list-name').text().trim();
-      const artist = $element.find('.cnt-list-row .cnt-list-artist').text().trim();
-
-      searchResults.push({ id, title, artist });
-   });
-
-   return searchResults;
 }
-*/
-
-// Exemplo de uso:
-//const input = {
-   //text: 'Em Fervente Oração'
-//};
-
-//searchLyrics()
-   //.then(results => {
-     // console.log(results);
-   //})
-   //.catch(error => {
-     // console.error('Error occurred:', error);
-  // });
